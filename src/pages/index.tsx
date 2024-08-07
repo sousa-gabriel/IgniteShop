b09160ee2165f41b24feb5fb51bbc6d2b7fb9d3d
sousa-gabriel <sousa-gabriel@hotmail.com>
2024-08-07 10:59:04 -0300

    implement `home and carrousel this app`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,68 @@
-export default function Home() {
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import { stripe } from "../lib/stripe";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
+import Stripe from "stripe";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 32,
+    },
+    loop: true,
+  });
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{`R$ ${product.price}`}</span>
+            </footer>
+          </Product>
+        );
+      })}
+    </HomeContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount! / 100,
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  height: "100vh",
+});
+
+export const Header = styled("header", {
+  width: "100%",
+  padding: "2rem",
+  maxWidth: 1180,
+  margin: "0 auto",
+});
