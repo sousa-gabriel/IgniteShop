f2aa8ac0d1ae68e3b3dfe1de6569cfd0953c5988
sousa-gabriel <sousa-gabriel@hotmail.com>
2024-08-07 10:57:43 -0300

    create `global styles`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { globalStyles } from "@/styles/global";
+import { AppProps } from "next/app";
+import logoImage from "../assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
-export default MyApp;
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImage} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
